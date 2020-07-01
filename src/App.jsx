7299c56a2b0353fa6e907c5a5e3de1ca25926bd6
Gustavo Leandro Gonçalves <guto_leandro95@hotmail.com>
2020-07-01 10:23:40 -0300

    start with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,66 @@
 import React from 'react'
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import { Nav, Navbar } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams
+  } from "react-router-dom";
+import Home from './components/Home';
+import Add from './components/Add';
 
-// Be sure to include styles at some point, probably during your bootstraping
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 
 const App = () => {
 
   return(
-    <SideNav
-    onSelect={(selected) => {
-        // Add your code here
-    }}
->
-    <SideNav.Toggle />
-    <SideNav.Nav defaultSelected="home">
-        <NavItem eventKey="home">
-            <NavIcon>
-                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText>
-                Home
-            </NavText>
-        </NavItem>
-        <NavItem eventKey="charts">
-            <NavIcon>
-                <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText>
-                Charts
-            </NavText>
-            <NavItem eventKey="charts/linechart">
-                <NavText>
-                    Line Chart
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="charts/barchart">
-                <NavText>
-                    Bar Chart
-                </NavText>
-            </NavItem>
-        </NavItem>
-    </SideNav.Nav>
-</SideNav>
+    <Router>
+        <Navbar bg="dark" defaultActiveKey="/" fluid collapseOnSelect>
+            <Nav>
+                <Nav.Item>
+                    <Navbar.Brand>
+                        <Link to="/">Home</Link>
+                    </Navbar.Brand>
+                </Nav.Item>
+            </Nav>
+                <Navbar.Collapse>
+                    <Nav className="ml-auto">
+                        <Nav.Item>
+                            <Navbar.Brand>
+                                <Link to="/add">Add</Link>
+                            </Navbar.Brand>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Navbar.Brand>
+                                <Link to="/edit">Edit</Link>
+                            </Navbar.Brand>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Navbar.Brand>
+                                <Link to="/remove">Remove</Link>
+                            </Navbar.Brand>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+        </Navbar>    
+
+        <Switch>
+            <Route path="/">
+                <Home/>
+            </Route>
+            <Route path="/add">
+                <Add/>
+            </Route>
+            <Route path="/edit">
+               
+            </Route>
+            <Route path="/remove">
+                
+            </Route>
+        </Switch>
+    </Router>  
   );
 }
 
